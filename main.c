f4ab44004b557e2bafa481dde92fe2f71cb6027f
Aaron Lang <aaronlang1042@gmail.com>
2013-03-04 14:31:03 -0600

    Implemented random

--- main.c
+++ main.c
@@ -15,10 +15,56 @@ how to use the page table and disk interfaces.
 #include <string.h>
 #include <errno.h>
 
+struct disk *disk;
+int framesLeft;
+int nframes;
+int npages;
+char *physmem;
+char *virtmem;
+char *method;
+
+void replace_random (struct page_table *pt, int page)
+{
+  int i = 0;
+  int frameReturned;
+  int bitsReturned;
+  int randNumPages = rand()%nframes;
+  while (i < npages){
+    page_table_get_entry(pt, i, &frameReturned, &bitsReturned);
+    if (bitsReturned != 0){
+	//printf("%i\n", randNumPages);
+	if(randNumPages == 0){
+	  disk_write(disk, i, &physmem[frameReturned*PAGE_SIZE]);
+	  disk_read(disk, page, &physmem[frameReturned*PAGE_SIZE]);
+	  page_table_set_entry(pt, page, frameReturned, PROT_READ);
+	  page_table_set_entry(pt, i, 0, 0);
+	  i = npages;
+	}else{
+	  randNumPages--;
+	}
+    }
+    i++;
+  }
+  
+}
 void page_fault_handler( struct page_table *pt, int page )
 {
-    page_table_set_entry(pt,page,page,PROT_READ|PROT_WRITE);
-    //HEHEHEHE
+  int frameReturned;
+  int bitsReturned;
+  page_table_get_entry(pt, page, &frameReturned, &bitsReturned);
+  if (bitsReturned == PROT_READ){
+    page_table_set_entry(pt,page,frameReturned,PROT_READ|PROT_WRITE);
+  }else if (framesLeft > 0){
+    framesLeft--;
+    page_table_set_entry(pt,page,framesLeft,PROT_READ);
+    disk_read(disk, page, &physmem[framesLeft*PAGE_SIZE]);
+  }else{
+      if (strcmp("rand", method) == 0){
+	replace_random(pt, page);	
+      }
+  }
+  
+  
 }
 
 int main( int argc, char *argv[] )
@@ -28,16 +74,17 @@ int main( int argc, char *argv[] )
 		return 1;
 	}
 
-	int npages = atoi(argv[1]);
-	int nframes = atoi(argv[2]);
+	npages = atoi(argv[1]);
+	nframes = atoi(argv[2]);
 	const char *program = argv[4];
+	method = argv[3];
+	framesLeft = atoi(argv[2]);
 
-	struct disk *disk = disk_open("myvirtualdisk",npages);
+	disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
 		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
 		return 1;
 	}
-
 
 	struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
 	if(!pt) {
@@ -45,9 +92,9 @@ int main( int argc, char *argv[] )
 		return 1;
 	}
 
-	char *virtmem = page_table_get_virtmem(pt);
+	virtmem = page_table_get_virtmem(pt);
 
-	char *physmem = page_table_get_physmem(pt);
+	physmem = page_table_get_physmem(pt);
 
 	if(!strcmp(program,"sort")) {
 		sort_program(virtmem,npages*PAGE_SIZE);
